a6266e3964d16014bcfcc0e47ad34928015349f9
Francisco Reyes <francisco@makingdevs.com>
2016-02-05 11:45:15 -0600

    Se crea clase comprobante

---
+++ src/main/groovy/com/makingdevs/Comprobante.groovy
@@ -0,0 +1,25 @@
+package com.makingdevs
+class Comprobante{
+	
+	String serie
+	String folio
+	Date fecha
+	String formaDePago
+	BigDecimal subTotal
+	BigDecimal descuento
+	Integer tipoCambio
+	String lugarExpedicion
+	String numCtaPago
+	String noCertificado
+	String certificado
+	String sello
+	Emisor emisor
+	Receptor receptor 
+	List<Concepto> conceptos=[] 
+	Impuesto impuesto
+	Complemento complemento
+
+
+
+
+}
