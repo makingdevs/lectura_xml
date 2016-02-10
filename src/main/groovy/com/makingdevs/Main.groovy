a5fcb1fae37a110039f01b333cdd30d4c0b2a681
Francisco Reyes <francisco@makingdevs.com>
2016-02-10 13:11:34 -0600

    Se anexa clase main

--- src/main/groovy/com/makingdevs/Main.groovy
+++ src/main/groovy/com/makingdevs/Main.groovy
@@ -1,61 +1,21 @@
 package com.makingdevs
 
 class Main{
-    static void main(def args){
-      /*
-      def concepto=new Concepto(valorUnitario:123)
-
-      def traslado=new Traslado(impuesto:"IVA")
-      def impuesto=new Impuesto(totalImpuestosTrasladado:123456)
-      impuesto.traslado=traslado
-
-      def timbreFiscalDigital=new TimbreFiscalDigital(selloSAT:"Sello SAT-XXX")
-      def complemento = new Complemento()
-      complemento.timbreFiscalDigital=timbreFiscalDigital
-
-      def comprobante=new Comprobante(folio:"2016/02/04:10:00")
-      def timbreFiscalDigital=new TimbreFiscalDigital(noCertificadoSAT:"1")
-      timbreFiscalDigital.uuid="wrqwerewqt"
-      comprobante.timbreFiscalDigital=timbreFiscalDigital
-      println (comprobante.timbreFiscalDigital.uuid)
-      comprobante.conceptos.add(concepto)
-      
-      println "Informacion de clases"
-      println (traslado.impuesto)
-      println (impuesto.totalImpuestosTrasladado)
-      println (impuesto.traslado?.impuesto)
-      println (concepto.valorUnitario)
-      println (complemento.timbreFiscalDigital?.selloSAT)
-      println (comprobante.folio)
-
-      comprobante.conceptos.each{
-        println(it.valorUnitario)
-      }
-      */
-
-      
-      def parseXML=new ParseXML()
-      def comprobante=new Comprobante()
-      //comprobante=parseXML.readFile("/Users/makingdevs/workspace/lectura_xml/1ORO9612152X9-FAOROABA000000005041.xml")
-      List<String> filesXML=parseXML.getFilesXML("/Users/makingdevs/workspace/lectura_xml/")
-      List<Comprobante> comprobantes=[]
+  static void main(def args){
+    def parseXML=new ParseXML()
+    def comprobante=new Comprobante()
+    
+    List<String> filesXML=parseXML.getFilesXML("/Users/makingdevs/workspace/lectura_xml/")
+    List<Comprobante> comprobantes=[]
       filesXML.each{file->
-        println file
         comprobantes.add(parseXML.readFile(file))
-      }
+    }
 
-      comprobantes.each{
-        println it.fecha
-      }
-      /*
-      comprobante.timbreFiscalDigital.getProperties().each{
-        println it
-      }
-      println "\n"
+    comprobantes.each{factura->
+      println "Factura informacion: "+factura.serie+" "+factura.subTotal+
+        " "+factura.total+" "+factura.conceptos.descripcion+
+        factura.conceptos.valorUnitario    
+    }
 
-      comprobante.impuesto.getProperties().each{
-        println it
-      }
-      */
   }
 }
