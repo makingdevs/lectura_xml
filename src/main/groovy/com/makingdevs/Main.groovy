aa69f05eba8d958796d3e338839418aaaca32083
Francisco Reyes <francisco@makingdevs.com>
2016-02-16 11:15:55 -0600

    Se agregan datos a mostrar de addenda

--- src/main/groovy/com/makingdevs/Main.groovy
+++ src/main/groovy/com/makingdevs/Main.groovy
@@ -5,12 +5,12 @@ class Main{
     def parseXML=new ParseXML()
     def comprobante=new Comprobante()
     
-    List<String> filesXML=parseXML.getFilesXML("C:\\Users\\PhenomII\\Documents\\home_work")
+    List<String> filesXML=parseXML.getFilesXML("/Users/makingdevs/workspace/facturas")
     List<Comprobante> comprobantes=[]
       filesXML.each{file->
         comprobantes.add(parseXML.readFile(file))
     }
-
+    
     comprobantes.each{factura->
       println "Factura informacion: "+factura.serie+" "+factura.subTotal+
         " "+factura.total+" "+factura.conceptos.descripcion+
@@ -19,6 +19,10 @@ class Main{
       println "Addenda: "+factura.addenda.estadoDeCuentaBancario.movimientoECB.descripcion
 
       print "Traslado: "+factura.impuesto.traslado.impuesto
+
+      factura.addenda.estadoDeCuentaBancario.getProperties().each{
+        print it.getValue()
+      }
     }
     
   }
