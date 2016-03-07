eb206df891d7c57681a8da2238d1becdd43b31d6
Francisco Reyes <francisco@makingdevs.com>
2016-03-07 17:22:23 -0600

    coding #4 Se agrega implementacion del metodo obtainVoucherFromInvoice

--- services/core/src/main/groovy/com/makingdevs/service/impl/InvoiceServiceImpl.groovy
+++ services/core/src/main/groovy/com/makingdevs/service/impl/InvoiceServiceImpl.groovy
@@ -10,7 +10,26 @@ import com.makingdevs.Addenda
 
 class InvoiceServiceImpl implements InvoiceService{
   Comprobante obtainVoucherFromInvoice(File invoice){
-
+    def voucher= new Comprobante()
+    def xml = new XmlSlurper().parseText(invoice.getText()).declareNamespace(
+      cfdi:"http://www.sat.gob.mx/cfd/3",
+      xsi:"http://www.w3.org/2001/XMLSchema-instance")
+    xml.attributes().each{attributeXML->
+      voucher.getProperties().each{attributeVoucher->
+        if(attributeXML.key.equalsIgnoreCase(attributeVoucher.key)){
+          if(attributeVoucher.key.equalsIgnoreCase("fecha")){
+            voucher[attributeVoucher.key]=Date.parse("yyyy-MM-dd'T'HH:mm:ss", attributeXML.value)
+          }
+          else if(["total","subTotal","descuento","tipoCambio"]*.toLowerCase().contains(attributeVoucher.key.toLowerCase())){ 
+            voucher[attributeVoucher.key]=new BigDecimal(attributeXML.value)
+          }
+          else{
+            voucher[attributeVoucher.key]=attributeXML.value
+          }
+        }
+      }
+    }
+    voucher
   }
 
   Emisor obtainTransmitterFromInvoice(File invoice){
