6cb4d16ae2aa05dea070f37d6d05292de0d1e087
Francisco Reyes <francisco@makingdevs.com>
2016-03-07 09:29:42 -0600

    Se agrega clase de estado de cuenta bancario

---
+++ src/main/groovy/com/makingdevs/EstadoDeCuentaBancario.groovy
@@ -0,0 +1,9 @@
+package com.makingdevs
+class EstadoDeCuentaBancario{
+	String version
+	String numeroCuenta
+	String nombreCliente
+	String periodo
+	String sucursal
+	List<MovimientoECB> movimientoECB=[]
+}
