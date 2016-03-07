838aa5627f3cb6050fb8bd407312ef307932ba25
Francisco Reyes <francisco@makingdevs.com>
2016-03-07 16:49:11 -0600

    coding #4 Se agrega implementacion del servicio invoice

---
+++ services/core/src/main/groovy/com/makingdevs/service/impl/InvoiceServiceImpl.groovy
@@ -0,0 +1,40 @@
+package com.makingdevs.service.impl
+import com.makingdevs.service.InvoiceService
+import com.makingdevs.Comprobante
+import com.makingdevs.Emisor
+import com.makingdevs.Receptor
+import com.makingdevs.Concepto
+import com.makingdevs.Impuesto
+import com.makingdevs.TimbreFiscalDigital
+import com.makingdevs.Addenda
+
+class InvoiceServiceImpl implements InvoiceService{
+  Comprobante obtainVoucherFromInvoice(File invoice){
+
+  }
+
+  Emisor obtainTransmitterFromInvoice(File invoice){
+
+  }
+
+  Receptor obtainReceiverFromInvoice(File invoice){
+
+  }
+
+  List<Concepto> obtainConceptsFromInvoice(File invoice){
+
+  }
+
+  Impuesto obtainTaxesFromInvoice(File invoice){
+
+  }
+
+  TimbreFiscalDigital obtainDigitalTaxStampFromInvoice(File invoice){
+
+  }
+
+  Addenda obtainAddendaFromInvoice(File invoice){
+
+  }
+  
+}
