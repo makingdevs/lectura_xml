cdc5a132516ea1c8d942bac252ed6537e0793845
Francisco Reyes <francisco@makingdevs.com>
2016-03-07 09:39:14 -0600

    Se agrega clase concepto

--- src/main/groovy/com/makingdevs/Concepto.groovy
+++ src/main/groovy/com/makingdevs/Concepto.groovy
@@ -1,9 +1,10 @@
 package com.makingdevs
+
 class Concepto{
-	Float cantidad
-	String unidad
-	String noIdentificacion
-	String descripcion
-	BigDecimal valorUnitario
-	BigDecimal importe
+  Float cantidad
+  String unidad
+  String noIdentificacion
+  String descripcion
+  BigDecimal valorUnitario
+  BigDecimal importe
 }
