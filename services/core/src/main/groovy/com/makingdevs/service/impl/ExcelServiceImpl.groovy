6cff62f7b13ce83f290e7c8ad1ee7e55c26de34f
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-03-08 16:11:04 -0600

    coding #7 creating workbook with the invoice info

--- services/core/src/main/groovy/com/makingdevs/service/impl/ExcelServiceImpl.groovy
+++ services/core/src/main/groovy/com/makingdevs/service/impl/ExcelServiceImpl.groovy
@@ -8,6 +8,7 @@ import org.apache.poi.ss.usermodel.CreationHelper
 import org.apache.poi.ss.usermodel.CellStyle
 import org.apache.poi.ss.usermodel.Row
 import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.xssf.usermodel.XSSFCell
 import com.makingdevs.Comprobante
 import com.makingdevs.service.ExcelService
 
@@ -24,18 +25,48 @@ class ExcelServiceImpl implements ExcelService{
     headerStyle.fillForegroundColor = IndexedColors.GREY_25_PERCENT.index
     headerStyle.fillPattern = CellStyle.SOLID_FOREGROUND
 
-    def headers = ["Fecha","Subtotal","Descuento","Impuesto",
-                   "Total","Emisor","Receptor","NoCertificado","Sello","Folio",
-                   "FormaDePago","Addenda","LugarExpedicion","TimbreFiscalDigital",
-                   "TipoDeComprobante","TipoDeCambio","Serie","Moneda","NumCtaPago",
-                   "Conceptos","Certificado","MetodoDePago"]
+    def fields = ["Fecha":invoice.fecha,
+                  "Subtotal":invoice.subTotal,
+                  "Descuento":invoice.descuento,
+                  "Impuesto":invoice.impuesto.totalImpuestosTrasladado,
+                  "Total":invoice.total,
+                  "Emisor":invoice.emisor.nombre,
+                  "Receptor":invoice.receptor.nombre,
+                  "No.Certificado":invoice.noCertificado,
+                  "Sello":invoice.sello,
+                  "Folio":invoice.folio,
+                  "FormaDePago":invoice.formaDePago,
+                  "Addenda":invoice.addenda.toString(),
+                  "LugarExpedicion":invoice.lugarExpedicion,
+                  "TimbreFiscalDigital":invoice.timbreFiscalDigital.uuid,
+                  "TipoDeComprobante":invoice.tipoDeComprobante,
+                  "TipoDeCambio":invoice.tipoCambio,
+                  "Serie":invoice.serie,
+                  "Moneda":invoice.moneda,
+                  "NumCtaPago":invoice.numCtaPago,
+                  "Conceptos":invoice.conceptos.toString(),
+                  "Certificado":invoice.certificado,
+                  "MetodoDePago":invoice.metodoDePago]
 
-    Row headerRow = sheet.createRow(0)
+    Row headerRow = sheet.createRow(sheet.lastRowNum)
 
-    headers.eachWithIndex{ header, index ->
-      Cell cell = headerRow.createCell(index)
-      cell.setCellStyle(headerStyle)
-      cell.setCellValue(header)
+    fields.eachWithIndex{ field, index ->
+      Cell headerCell = headerRow.createCell(index)
+      headerCell.cellStyle = headerStyle
+      headerCell.cellValue = field.key
+    }
+
+    Row invoiceRow = sheet.createRow(sheet.lastRowNum+1)
+    Cell invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum+1)
+
+    fields.each{ field ->
+      if(field.value?.class?.simpleName == BigDecimal.class.simpleName)
+        invoiceCell.cellType = XSSFCell.CELL_TYPE_NUMERIC
+      else if(field.value?.class?.simpleName == Date.class.simpleName)
+        invoiceCell.cellStyle = dateStyle
+
+      invoiceCell.cellValue = field.value
+      invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum)
     }
 
     workbook
