824fdf4cd35937e2a0f09bd5c98d486bb211df8d
Francisco Reyes <francisco@makingdevs.com>
2016-03-08 10:59:06 -0600

    codign #4 Se agrega implementacion del metodo obtainTransmitterFromInvoice

--- services/core/src/main/groovy/com/makingdevs/service/impl/InvoiceServiceImpl.groovy
+++ services/core/src/main/groovy/com/makingdevs/service/impl/InvoiceServiceImpl.groovy
@@ -7,6 +7,8 @@ import com.makingdevs.Concepto
 import com.makingdevs.Impuesto
 import com.makingdevs.TimbreFiscalDigital
 import com.makingdevs.Addenda
+import com.makingdevs.Direccion
+import com.makingdevs.RegimenFiscal
 
 class InvoiceServiceImpl implements InvoiceService{
   Comprobante obtainVoucherFromInvoice(File invoice){
@@ -33,7 +35,46 @@ class InvoiceServiceImpl implements InvoiceService{
   }
 
   Emisor obtainTransmitterFromInvoice(File invoice){
-
+    def voucher= new Comprobante()
+    def emisor=new Emisor()
+    def domicilioFiscal=new Direccion()
+    def lugarExpedicion=new Direccion()
+    def regimenFiscal=new RegimenFiscal()
+    
+    def xml = new XmlSlurper().parseText(invoice.getText()).declareNamespace(
+      cfdi:"http://www.sat.gob.mx/cfd/3",
+      xsi:"http://www.w3.org/2001/XMLSchema-instance")
+    xml.Emisor.each{atributo->
+      emisor.rfc=atributo.@rfc
+      emisor.nombre=atributo.@nombre
+      regimenFiscal.regimen=atributo.RegimenFiscal.@Regimen
+      emisor.regimen=regimenFiscal
+      voucher.emisor=emisor
+    }
+    xml.ExpedidoEn.each{atributo->
+      lugarExpedicion.calle=atributo.@calle
+      lugarExpedicion.noExterior=atributo.@noExterior
+      lugarExpedicion.colonia=atributo.@colonia
+      lugarExpedicion.municipio=atributo.@municipio
+      lugarExpedicion.estado=atributo.@estado
+      lugarExpedicion.pais=atributo.@pais
+      lugarExpedicion.codigoPostal=atributo.@codigoPostal
+      emisor.lugarExpedicion=lugarExpedicion
+      voucher.emisor=emisor
+    }
+    xml.Emisor.DomicilioFiscal.each{atributo->
+      domicilioFiscal.calle=atributo.@calle
+      domicilioFiscal.noExterior=atributo.@noExterior
+      domicilioFiscal.noInterior=atributo.@noInterior
+      domicilioFiscal.colonia=atributo.@colonia
+      domicilioFiscal.municipio=atributo.@municipio
+      domicilioFiscal.estado=atributo.@estado
+      domicilioFiscal.pais=atributo.@pais
+      domicilioFiscal.codigoPostal=atributo.@codigoPostal
+      emisor.domicilioFiscal=domicilioFiscal
+      voucher.emisor=emisor
+    }
+    emisor
   }
 
   Receptor obtainReceiverFromInvoice(File invoice){
