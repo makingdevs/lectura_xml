66ae4821f036e6c664a3fe18cc9df4e840acd5c2
Gamaliel Jimenez García <egjimenezg@gmail.com>
2016-03-08 18:22:10 -0600

    coding #7 updating method implementations

--- services/core/src/main/groovy/com/makingdevs/service/impl/ExcelServiceImpl.groovy
+++ services/core/src/main/groovy/com/makingdevs/service/impl/ExcelServiceImpl.groovy
@@ -14,70 +14,93 @@ import com.makingdevs.service.ExcelService
 
 class ExcelServiceImpl implements ExcelService{
 
-  XSSFWorkbook generateExcelWorkbookWithInvoiceDetail(Comprobante invoice){
+  XSSFWorkbook generateExcelWorkbook(){
     XSSFWorkbook workbook = new XSSFWorkbook()
     XSSFSheet sheet=workbook.createSheet("Página_1")
-    CreationHelper createHelper = workbook.getCreationHelper()
-    XSSFCellStyle dateStyle = workbook.createCellStyle()
-    XSSFCellStyle headerStyle = workbook.createCellStyle()
-
-    dateStyle.dataFormat = createHelper.createDataFormat().getFormat("dd-MM-YYYY HH:mm")
-    headerStyle.fillForegroundColor = IndexedColors.GREY_25_PERCENT.index
-    headerStyle.fillPattern = CellStyle.SOLID_FOREGROUND
-
-    def fields = ["Fecha":invoice.fecha,
-                  "Subtotal":invoice.subTotal,
-                  "Descuento":invoice.descuento,
-                  "Impuesto":invoice.impuesto.totalImpuestosTrasladado,
-                  "Total":invoice.total,
-                  "Emisor":invoice.emisor.nombre,
-                  "Receptor":invoice.receptor.nombre,
-                  "No.Certificado":invoice.noCertificado,
-                  "Sello":invoice.sello,
-                  "Folio":invoice.folio,
-                  "FormaDePago":invoice.formaDePago,
-                  "Addenda":invoice.addenda.toString(),
-                  "LugarExpedicion":invoice.lugarExpedicion,
-                  "TimbreFiscalDigital":invoice.timbreFiscalDigital.uuid,
-                  "TipoDeComprobante":invoice.tipoDeComprobante,
-                  "TipoDeCambio":invoice.tipoCambio,
-                  "Serie":invoice.serie,
-                  "Moneda":invoice.moneda,
-                  "NumCtaPago":invoice.numCtaPago,
-                  "Conceptos":invoice.conceptos.toString(),
-                  "Certificado":invoice.certificado,
-                  "MetodoDePago":invoice.metodoDePago]
-
-    Row headerRow = sheet.createRow(sheet.lastRowNum)
-
-    fields.eachWithIndex{ field, index ->
-      Cell headerCell = headerRow.createCell(index)
-      headerCell.cellStyle = headerStyle
-      headerCell.cellValue = field.key
-    }
-
-    Row invoiceRow = sheet.createRow(sheet.lastRowNum+1)
-    Cell invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum+1)
-
-    fields.each{ field ->
-      if(field.value?.class?.simpleName == BigDecimal.class.simpleName)
-        invoiceCell.cellType = XSSFCell.CELL_TYPE_NUMERIC
-      else if(field.value?.class?.simpleName == Date.class.simpleName)
-        invoiceCell.cellStyle = dateStyle
-
-      invoiceCell.cellValue = field.value
-      invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum)
-    }
-
     workbook
   }
 
-  XSSFWorkbook generateFileExcelWithAllInvoices(List<Comprobante> invoices){
+  void addInvoiceDetailToWorkbook(Comprobante invoice,XSSFWorkbook workbook){
+    CreationHelper createHelper = workbook.getCreationHelper()
+    XSSFSheet sheet = workbook.getSheetAt(0)
+    XSSFCellStyle dateStyle = workbook.createCellStyle()
+    dateStyle.dataFormat = createHelper.createDataFormat().getFormat("dd-MM-YYYY HH:mm")
+
+    def fields = [invoice.fecha,
+                  invoice.subTotal,
+                  invoice.descuento,
+                  invoice.impuesto.totalImpuestosTrasladado,
+                  invoice.total,
+                  invoice.emisor.nombre,
+                  invoice.receptor.nombre,
+                  invoice.noCertificado,
+                  invoice.sello,
+                  invoice.folio,
+                  invoice.formaDePago,
+                  invoice.addenda.toString(),
+                  invoice.lugarExpedicion,
+                  invoice.timbreFiscalDigital.uuid,
+                  invoice.tipoDeComprobante,
+                  invoice.tipoCambio,
+                  invoice.serie,
+                  invoice.moneda,
+                  invoice.numCtaPago,
+                  invoice.conceptos.toString(),
+                  invoice.certificado,
+                  invoice.metodoDePago]
+
+    Row invoiceRow = sheet.createRow(sheet.getPhysicalNumberOfRows())
+    Cell invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum+1)
+
+    fields.each{ field ->
+      if(field?.class?.simpleName == BigDecimal.class.simpleName)
+        invoiceCell.cellType = XSSFCell.CELL_TYPE_NUMERIC
+      else if(field?.class?.simpleName == Date.class.simpleName)
+        invoiceCell.cellStyle = dateStyle
+
+      invoiceCell.cellValue = field
+      invoiceCell = invoiceRow.createCell(invoiceRow.lastCellNum)
+    }
+  }
+
+  XSSFWorkbook generateWorkbookWithAllInvoices(List<Comprobante> invoices){
+    XSSFWorkbook workbook = generateExcelWorkbook()
+    addHeadersToWorkbook(workbook)
+
+    invoices.each{ invoice ->
+      addInvoiceDetailToWorkbook(invoice,workbook)
+    }
+
+    FileOutputStream out = new FileOutputStream(new File("Test_Excel.xlsx"))
+    workbook.write(out)
+    out.close()
+    workbook
+  }
+
+  XSSFWorkbook generateFileExcelWithAddendaInvoice(Comprobante invoice){
 
   }
 
-  XSSFWorkbook generateFileExcelWithDetailInvoice(Comprobante invoice){
+  private void addHeadersToWorkbook(XSSFWorkbook workbook){
+    XSSFSheet sheet = workbook.getSheetAt(0)
 
+    def headers = ["Fecha","Subtotal","Descuento","Impuesto","Total",
+                   "Emisor","Receptor","No.Certificado","Sello",
+                   "Folio","FormaDePago","Addenda","LugarExpedicion",
+                   "TimbreFiscalDigital","TipoDeComprobante","TipoDeCambio",
+                   "Serie","Moneda","NumCtaPago","Conceptos",
+                   "Certificado","MetodoDePago"]
+
+    Row headerRow = sheet.createRow(sheet.getPhysicalNumberOfRows())
+    XSSFCellStyle headerStyle = workbook.createCellStyle()
+    headerStyle.fillForegroundColor = IndexedColors.GREY_25_PERCENT.index
+    headerStyle.fillPattern = CellStyle.SOLID_FOREGROUND
+
+    headers.eachWithIndex{ header,index ->
+      Cell headerCell = headerRow.createCell(index)
+      headerCell.cellStyle = headerStyle
+      headerCell.cellValue = header
+    }
   }
 
 }
