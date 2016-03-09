3e5333bb344ee6d6b7e71fbb4c942d13b9b3216d
Francisco Reyes <francisco@makingdevs.com>
2016-03-09 15:26:10 -0600

    Se agrega archivo para servicio web

---
+++ webservices/src/main/webapp/WEB-INF/groovy/createInvoiceFromFile.groovy
@@ -0,0 +1,13 @@
+html.html{
+  head {
+    title 'WebService'
+  }  
+  body {
+    h3   'Bienvenido'
+    def fecha = new Date()
+    p fecha.format("dd MMMM yyyy HH:mm:ss")
+    String msn="Hola!"
+    p msn
+  }
+}
+
