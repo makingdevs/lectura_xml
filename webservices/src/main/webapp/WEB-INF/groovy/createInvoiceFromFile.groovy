58ea07c3c1a77b88e833bf1bd44ea7c50556389f
Francisco Reyes <francisco@makingdevs.com>
2016-03-10 13:55:26 -0600

    codign #6 Se agrega la implementacion del servicio, asi como mostrar datos del comprobante

--- webservices/src/main/webapp/WEB-INF/groovy/createInvoiceFromFile.groovy
+++ webservices/src/main/webapp/WEB-INF/groovy/createInvoiceFromFile.groovy
@@ -1,6 +1,15 @@
+import com.makingdevs.service.impl.InvoiceServiceImpl
+import com.makingdevs.Comprobante
+
 def method = request.method
+File file= new File("invoice.xml")
 InputStream inputStream=request.inputStream
-OutputStream outputStream = new FileOutputStream("woorbook.xml")
+OutputStream outputStream = new FileOutputStream(file)
 while ((byteInput = inputStream.read()) != -1){
   outputStream.write(byteInput)
 }
+
+InvoiceServiceImpl invoiceService = new InvoiceServiceImpl()
+Comprobante comprobante=invoiceService.obtainVoucherFromInvoice(file)
+
+println "Valores:. "+comprobante.total
