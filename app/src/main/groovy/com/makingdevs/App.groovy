c29e68d389b35646b8fa8c9ff7252b37e3d4d2aa
neodevelop <neodevelop@gmail.com>
2016-03-10 13:39:20 -0600

    #10 Mostrando modal para seleccionar directorio

--- app/src/main/groovy/com/makingdevs/App.groovy
+++ app/src/main/groovy/com/makingdevs/App.groovy
@@ -1,7 +1,28 @@
 package com.makingdevs
 
+import javax.swing.*
+import javax.swing.filechooser.*
+import groovy.swing.SwingBuilder
+
 class App {
-  static main(String[] args){
+  static void main(String[] args){
+    chooseDirectory()
+  }
+
+  static chooseDirectory(){
+    def initialPath = System.getProperty("user.dir")
+    JFileChooser fc = new JFileChooser(initialPath)
+    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
+    int action = fc.showOpenDialog( null )
+    switch ( action ) {
+      case JFileChooser.APPROVE_OPTION:
+      File file = fc.getSelectedFile()
+      def path =  fc.getCurrentDirectory().getAbsolutePath()
+      println "path="+path+"\nfile name="+file.toString()
+      break
+      case JFileChooser.CANCEL_OPTION:
+      case JFileChooser.ERROR_OPTION:
+      break
+    }
   }
 }
-
