5aa1b991dc831ab9b27809150cb896d469313ff7
neodevelop <neodevelop@gmail.com>
2016-03-10 12:27:40 -0600

    #10 Agregando jAnsi para colorizar la terminal

--- app/src/main/groovy/com/makingdevs/App.groovy
+++ app/src/main/groovy/com/makingdevs/App.groovy
@@ -1,6 +1,15 @@
 package com.makingdevs
 
+import org.fusesource.jansi.AnsiConsole
+import static org.fusesource.jansi.Ansi.*
+import static org.fusesource.jansi.Ansi.Color.*
+
 class App {
-  static main(String[] args){
+  static void main(String[] args){
+    AnsiConsole.systemInstall()
+    println "${ansi().eraseScreen().render('@|red Hello|@ @|green World|@')}"
+    println "What is your name?"
+    println "Your name is ${System.in.newReader().readLine()}"
   }
 }
+
