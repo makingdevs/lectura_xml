9ffcc646912920ab7ae6636188c9166736f964de
neodevelop <neodevelop@gmail.com>
2016-03-10 13:54:22 -0600

    #10 reestructurando

--- app/src/main/groovy/com/makingdevs/App.groovy
+++ app/src/main/groovy/com/makingdevs/App.groovy
@@ -1,28 +1,26 @@
 package com.makingdevs
 
-import javax.swing.*
+import javax.swing.*;
 import javax.swing.filechooser.*
 import groovy.swing.SwingBuilder
 
 class App {
   static void main(String[] args){
-    chooseDirectory()
-  }
-
-  static chooseDirectory(){
-    def initialPath = System.getProperty("user.dir")
-    JFileChooser fc = new JFileChooser(initialPath)
-    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY)
-    int action = fc.showOpenDialog( null )
-    switch ( action ) {
+    def initialPath = System.getProperty("user.dir");
+    JFileChooser fc = new JFileChooser(initialPath);
+    fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+    int result = fc.showOpenDialog( null );
+    switch ( result )
+    {
       case JFileChooser.APPROVE_OPTION:
-      File file = fc.getSelectedFile()
-      def path =  fc.getCurrentDirectory().getAbsolutePath()
-      println "path="+path+"\nfile name="+file.toString()
-      break
+      File file = fc.getSelectedFile();
+
+      def path =  fc.getCurrentDirectory().getAbsolutePath();
+      println "path="+path+"\nfile name="+file.toString();
+      break;
       case JFileChooser.CANCEL_OPTION:
       case JFileChooser.ERROR_OPTION:
-      break
+      break;
     }
   }
 }
