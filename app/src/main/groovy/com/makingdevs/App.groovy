1de903e605556573b1930bf10ce497450413989f
neodevelop <neodevelop@gmail.com>
2016-03-10 12:27:45 -0600

    Revert "#10 Agregando jAnsi para colorizar la terminal"
    This reverts commit 0aba55ef3af6ec26bda45680012255b66b3bb82d.

--- app/src/main/groovy/com/makingdevs/App.groovy
+++ app/src/main/groovy/com/makingdevs/App.groovy
@@ -1,15 +1,7 @@
 package com.makingdevs
 
-import org.fusesource.jansi.AnsiConsole
-import static org.fusesource.jansi.Ansi.*
-import static org.fusesource.jansi.Ansi.Color.*
-
 class App {
-  static void main(String[] args){
-    AnsiConsole.systemInstall()
-    println "${ansi().eraseScreen().render('@|red Hello|@ @|green World|@')}"
-    println "What is your name?"
-    println "Your name is ${System.in.newReader().readLine()}"
+  static main(String[] args){
   }
 }
 
