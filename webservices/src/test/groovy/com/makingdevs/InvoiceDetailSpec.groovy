906eb15bedfa671999e7d8409220ccf068596491
Francisco Reyes <francisco@makingdevs.com>
2016-03-14 16:33:50 -0600

    codign #12 Se agrega prueba para la peticion get, regresa estado de la peticon en OK

--- webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
+++ webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
@@ -3,12 +3,10 @@ import spock.lang.Specification
 import java.lang.Void as Should
 import wslite.rest.*
 import com.makingdevs.*
+import groovy.json.JsonSlurper
 
 class InvoiceDetailSpec extends Specification{
-  //InvoiceParser invoiceDetail
-  def setup (){
-    //invoiceDetail = new InvoiceParser()
-  }
+  
   Should "Verify what response to be POST with Wslite retrieve JSON"(){
     given:
       File invoice = new File(this.class.classLoader.getResource("factura.xml").getFile())
@@ -23,6 +21,20 @@ class InvoiceDetailSpec extends Specification{
                                                 }
     then:
       responsePOST.contentAsString
-      println responsePOST.contentAsString
   }
+
+  Should "Verify what response to be GET with Wslite retrieve STATUS=OK"(){
+    given:
+      def slurperJson = new JsonSlurper()
+      def client = new RESTClient("http://localhost:1234/webservices/invoiceDetail.groovy")
+    when:
+      def responseGET = client.get(headers:[
+                                            "Content-Type":"application/octet-stream",
+                                            "Accept":"application/json"
+                                            ]) {}
+      def statusWebService = slurperJson.parseText(responseGET.contentAsString)
+    then:
+      statusWebService.status=="OK"
+  }
+
 }
