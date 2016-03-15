93e2d4a8f52a82af74e23ffb2c3c96fca4ded8d9
Francisco Reyes <francisco@makingdevs.com>
2016-03-15 12:54:05 -0600

    codign #12 Se agrega metodo para iniciar servidor jetty, ademas de cargar recurso de factura XML

--- webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
+++ webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
@@ -8,12 +8,15 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.*
 import groovy.servlet.*
 
-//import static org.eclipse.jetty.servlet.ServletContextHandler.*
+import static org.eclipse.jetty.servlet.ServletContextHandler.*
 
 class InvoiceDetailSpec extends Specification{
-  
+  def setup() {
+    startJetty()
+  }
   Should "Verify what response to be POST with Wslite retrieve JSON"(){
     given:
+      //startJetty()
       File invoice = new File(this.class.classLoader.getResource("factura.xml").getFile())
       def client = new RESTClient("http://localhost:1234/webservices/invoiceDetail.groovy")
     when:
@@ -27,15 +30,12 @@ class InvoiceDetailSpec extends Specification{
       
     then:
       responsePOST.contentAsString
+      //println responsePOST.contentAsString
   }
-
+  
   Should "Verify what response to be GET with Wslite retrieve STATUS=OK"(){
     given:
-      def server = new Server(1234)
-      def context = new ServletContextHandler(server, "/", SESSIONS)
-      context.resourceBase = "."
-      context.addServlet(GroovyServlet, "*.groovy")
-      server.start()
+      //startJetty()
       def slurperJson = new JsonSlurper()
       def client = new RESTClient("http://localhost:1234/webservices/invoiceDetail.groovy")
     when:
@@ -48,5 +48,13 @@ class InvoiceDetailSpec extends Specification{
       statusWebService.status=="OK"
   }
 
+  def startJetty() {
+    def server = new Server(1234)
+    def context = new ServletContextHandler(server, "/", SESSIONS)
+    context.resourceBase = "/webservices"
+    context.setContextPath("/webservices/src/main/webapp/com/WEB-INF/groovy")
+    context.addServlet(GroovyServlet, "*.groovy")
+    server.start()
+  }
 
 }
