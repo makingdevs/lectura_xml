a6adf151ff88b00262a2f8367e8944a46ad65cd1
Francisco Reyes <francisco@makingdevs.com>
2016-03-18 11:19:00 -0600

    Se cambia el nombre del servicio a consumir para que pase la prueba

--- webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
+++ webservices/src/test/groovy/com/makingdevs/InvoiceDetailSpec.groovy
@@ -33,7 +33,7 @@ class InvoiceDetailSpec extends Specification{
   Should "Verify what response to be POST with Wslite retrieve JSON"(){
     given:
       File invoice = new File(this.class.classLoader.getResource("factura.xml").getFile())
-      def client = new RESTClient("http://localhost:1234/invoiceDetail.groovy")
+      def client = new RESTClient("http://localhost:1234/InvoiceDetail.groovy")
     when:
       def responsePOST = client.post(headers:[
                                               "Content-Type":"application/octet-stream",
@@ -46,11 +46,11 @@ class InvoiceDetailSpec extends Specification{
     then:
       responsePOST.contentAsString
   }
-
+  
   Should "Verify what response to be GET with Wslite retrieve STATUS=OK"(){
     given:
       def slurperJson = new JsonSlurper()
-      def client = new RESTClient("http://localhost:1234/invoiceDetail.groovy")
+      def client = new RESTClient("http://localhost:1234/InvoiceDetail.groovy")
     when:
       def responseGET = client.get(headers:[
                                             "Content-Type":"application/octet-stream",
