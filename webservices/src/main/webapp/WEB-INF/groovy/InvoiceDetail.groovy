7108865b80ff9236f973a2d0343bea48d4f41d35
Francisco Reyes <francisco@makingdevs.com>
2016-03-20 19:04:10 -0600

    coding #15 Se habilita peticion por get al webservices

--- webservices/src/main/webapp/WEB-INF/groovy/InvoiceDetail.groovy
+++ webservices/src/main/webapp/WEB-INF/groovy/InvoiceDetail.groovy
@@ -20,9 +20,6 @@ try{
 
   def contentType = headers.find{ k,v -> k.toLowerCase() == 'content-type' }?.value
 
-  if(contentType != "application/octet-stream")
-    throw new RuntimeException("Please use 'application/octet-stream' header")
-
   if(method.toLowerCase()=="post"){
     InvoiceParser invoiceParser =new InvoiceParser()
     File file=invoiceParser.getFileFromInputStream(request.inputStream)
