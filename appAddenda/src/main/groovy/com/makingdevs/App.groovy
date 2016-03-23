54f50c32c5ac0389f7c908befbfd39d9e0841934
Francisco Reyes <francisco@makingdevs.com>
2016-03-23 12:54:36 -0600

    coding #17 Se agrega app swing con groovy para el metodo de generar addenda

---
+++ appAddenda/src/main/groovy/com/makingdevs/App.groovy
@@ -0,0 +1,32 @@
+package com.makingdevs
+
+import javax.swing.*
+import javax.swing.filechooser.*
+import groovy.swing.SwingBuilder
+import com.makingdevs.accounting.impl.*
+import com.makingdevs.accounting.*
+
+class App {
+
+  static InvoiceFileOperation invoiceFileOperation = new InvoiceFileOperationImpl()
+
+  static void main(String[] args){
+    def initialPath = System.getProperty("user.dir")
+    JFileChooser fc = new JFileChooser(initialPath)
+    FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos XML", "xml")
+    fc.setFileFilter(filter)
+    fc.setFileSelectionMode(JFileChooser.FILES_ONLY)
+    int result = fc.showOpenDialog( null )
+    switch ( result ) {
+      case JFileChooser.APPROVE_OPTION:
+        File file = fc.selectedFile
+        def path =  fc.currentDirectory.absolutePath
+        File excelFile = invoiceFileOperation.createInvoiceWithAddendaFile(file.canonicalPath)
+        excelFile.renameTo(new File(file.canonicalPath, excelFile.name))
+      break
+      case JFileChooser.CANCEL_OPTION:
+      case JFileChooser.ERROR_OPTION:
+      break
+    }
+  }
+}
