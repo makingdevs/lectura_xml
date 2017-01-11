773f6f70e666cac5105ce8318562fa8275338597
neodevelop <juan@makingdevs.com>
2017-01-10 18:18:36 -0600

    #23 Agregando una app alterna para hacer la conciliación

---
+++ appReconciliation/src/main/groovy/com/makingdevs/App.groovy
@@ -0,0 +1,14 @@
+package com.makingdevs
+
+import javax.swing.*
+import javax.swing.filechooser.*
+import groovy.swing.SwingBuilder
+import com.makingdevs.accounting.impl.*
+import com.makingdevs.accounting.*
+
+class App {
+
+  static main(String[] args){
+    println args
+  }
+}
